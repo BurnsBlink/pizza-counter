ab62733a32d0aa0cca1c746bbd8ee923f5f37290
Sam Burns <samuel.burns.25@gmail.com>
2021-05-12 14:36:39 -0400

    Implement count api (#3)

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,25 @@
 import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import App from './App';
 
-afterEach(cleanup);
+describe('App', () => {
+  beforeEach(() => {
+    render(<App />);
+  })
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Add a PIZZA!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  afterEach(cleanup);
 
-test('renders learn react link00', () => {
-  const { getByTestId } = render(<App />)
-  const count = getByTestId(`count`)
-  const add = screen.getByText(/Add a PIZZA!/i)
+  test('renders learn react link', () => {
+    const linkElement = screen.getByText(/Add a PIZZA!/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 
-  fireEvent.click(add)
-  expect(count.innerHTML).toBe('1')
-});
+  test('should update the count by 1 when clicked', () => {
+    const add = screen.getByText(/Add a PIZZA!/i)
+    const count = screen.getByTestId(`count`)
+  
+    fireEvent.click(add)
+    setTimeout(() => {
+      expect(count.innerHTML).toBe('Total number of pizzas clicked: <span id=\"pizza-count\">1!</span>')
+    }, 1000);
+  });
+})
