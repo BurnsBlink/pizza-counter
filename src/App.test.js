1ba2f8410b018916d183be7a6d8b92afcf44d302
Sam <samuel.burns.25@gmail.com>
2021-05-12 09:14:59 -0400

    Init set up & stripped down ReactApp

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Counter Clicker button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Counter Clicker/i);
   expect(linkElement).toBeInTheDocument();
 });
