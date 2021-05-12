9061fff4332d762dfb7f439bfd4bbb8f84844167
Sam Burns <samuel.burns.25@gmail.com>
2021-05-12 11:20:17 -0400

    Implement simple counter +1 button (#2)

--- solution.md
+++ solution.md
@@ -1,1 +1,13 @@
-### Coming Soon
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.
+
+### `Coming Soon`
+
+* Lint & E2E testing
+* Mobile friendly display
+* Options for more than 1 pizza(clicks)
+* Use local png/jpg instead of url links
--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.scss';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Fearless
-      </header>
-      <div class="body">
-        <button>Counter Clicker
-        </button>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0};
+
+    this.increment = this.increment.bind(this)
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div id="count-block" data-testid="count">
+            {this.state.count}
+        </div>
+        <button onClick={this.increment}>Add a Pizza!</button>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
