49436621cc9cde3d561837b1f28732734d40fd79
Sam <samuel.burns.25@gmail.com>
2021-05-12 11:16:44 -0400

    Implement simple counter +1 button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import App from './App';
 
-test('renders Counter Clicker button', () => {
+afterEach(cleanup);
+
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Counter Clicker/i);
+  const linkElement = screen.getByText(/Add a PIZZA!/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link00', () => {
+  const { getByTestId } = render(<App />)
+  const count = getByTestId(`count`)
+  const add = screen.getByText(/Add a PIZZA!/i)
+
+  fireEvent.click(add)
+  expect(count.innerHTML).toBe('1')
 });
